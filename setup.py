40d8562033eaca4770e3d50f2e63b2f40458db07
Christopher Pickering <cpickering@rhc.net>
2021-07-12 15:26:56 -0500

    updated version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def long_description():
 
 setup(
     name="djlint",
-    version="0.0.6",
+    version="0.0.7",
     author="Christopher Pickering",
     author_email="cpickering@rhc.net",
     description="Django Template Linter",
