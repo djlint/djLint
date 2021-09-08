53c6cb425e1c109a73f5609c9e7d159b6e5888ba
Christopher Pickering <cpickering@rhc.net>
2021-09-08 11:53:31 +0200

    updated version

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 project = "djlint"
 copyright = "2021, Riverside Healthcare"
 author = "Christopher Pickering"
-release = "0.3.5"
+release = "0.3.6"
 version = release
 
 # -- General configuration ---------------------------------------------------
