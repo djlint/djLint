7c1b2ebcf8cf224caacdf295c0dfe431453e9d99
Christopher Pickering <cpickering@rhc.net>
2021-10-07 16:04:27 +0200

    added rule 26 or empty id and class tags. closes #87

--- tests/test_linter.py
+++ tests/test_linter.py
@@ -371,6 +371,23 @@ def test_H025(runner: CliRunner, tmp_file: TextIO) -> None:
     assert "H025" not in result.output
 
 
+def test_H026(runner: CliRunner, tmp_file: TextIO) -> None:
+    write_to_file(tmp_file.name, b'<asdf id="" >')
+    result = runner.invoke(djlint, [tmp_file.name])
+    assert result.exit_code == 1
+    assert "H026" in result.output
+
+    write_to_file(tmp_file.name, b"<asdf id >")
+    result = runner.invoke(djlint, [tmp_file.name])
+    assert result.exit_code == 1
+    assert "H026" in result.output
+
+    write_to_file(tmp_file.name, b'<asdf class="" >')
+    result = runner.invoke(djlint, [tmp_file.name])
+    assert result.exit_code == 1
+    assert "H026" in result.output
+
+
 def test_rules_not_matched_in_ignored_block(
     runner: CliRunner, tmp_file: TextIO
 ) -> None:
