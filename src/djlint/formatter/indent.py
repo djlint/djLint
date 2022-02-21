249034c72a9a881a523da6a711b1883ebbdc18aa
Christopher Pickering <christopher.pickering@keemail.me>
2022-02-21 17:09:45 -0600

    started new formatter

--- src/djlint/formatter/indent.py
+++ src/djlint/formatter/indent.py
@@ -1,245 +1,171 @@
-"""djLint add indentation to html."""
+from .parser import HTMLParser
+import re
+rawcode = """<P>
+    some nice text <a href="this">asdf</a>, ok
+</p>"""
+#rawcode = """<div class="a" id="b"></div>"""
 
-from functools import partial
+def indent_html(rawcode, config):
 
-import regex as re
+    elements = []
+    class MyHTMLParser(HTMLParser):
 
-from ..helpers import (
-    inside_ignored_block,
-    is_ignored_block_closing,
-    is_ignored_block_opening,
-    is_safe_closing_tag,
-)
-from ..settings import Config
-from .attributes import format_attributes
+        def handle_starttag(self, tag, attrs, tag_type):
+            elements.append({"location": "START", "type":tag_type, "tag":tag, "attrs": attrs})
+            #print("Encountered a start tag:", tag, " with attrs: ", attrs, " type: ", tag_type)
+
+        def handle_endtag(self, tag, tag_type):
+            elements.append({"location": "END", "type":tag_type, "tag":tag})
+            #print("Encountered an end tag :", tag, " type: ", tag_type)
+
+        def handle_data(self, data):
+            elements.append({"data":data})
+            #print("Encountered some data  :", data)
+
+        def handle_comment(self, data, tag_type):
+            elements.append({"comment": data, "type":tag_type})
 
 
-def indent_html(rawcode: str, config: Config) -> str:
-    """Indent raw code."""
-    rawcode_flat_list = re.split("\n", rawcode)
+    #print(data)
 
-    indent = config.indent
 
-    beautified_code = ""
-    indent_level = 0
-    is_raw_first_line = False
-    is_block_raw = False
+    ## pop front mater
+    rawcode = rawcode.strip()
 
-    slt_html = config.indent_html_tags
+    front_matter = re.search(r'^---[\s\S]+?---\S*', rawcode)
 
-    # here using all tags cause we allow empty tags on one line
-    always_self_closing_html = config.always_self_closing_html_tags
+    if front_matter:
+        front_matter = front_matter.group()
+        rawcode = rawcode.replace(front_matter,"")
+        front_matter = front_matter.strip() + "\n"
+    else:
+        front_matter = ""
 
-    # here using all tags cause we allow empty tags on one line
-    slt_template = config.optional_single_line_template_tags
+    p = MyHTMLParser()
+    p.feed(rawcode)
+    p.close()
 
-    # nested ignored blocks..
+
+    indent_html_tags = ["div", "p", "dd"]
+    ignored_html_tags = ["pre","code", 'textarea', 'script']
+    inline_blocks = ["h1","h2","h3","h4","h5",'h6', "a", "span", "input", "small"]
+    indent_temp_tags = ["if"]
+    indent_block = False
+    current_block = "tag"
+
+    indent = "    "
+    level = 0
     ignored_level = 0
+    inline_level = 0
+    is_inline = False
 
-    for item in rawcode_flat_list:
-        # if a raw tag first line
-        if not is_block_raw and is_ignored_block_opening(config, item):
-            is_raw_first_line = True
+    def breakbefore(html):
 
-        # if a raw tag then start ignoring
-        if is_ignored_block_opening(config, item):
-            is_block_raw = True
-            ignored_level += 1
+        return bool(re.search(r"\n[ \t]*$",html, re.M))
 
-        if is_safe_closing_tag(config, item):
-            ignored_level -= 1
-            ignored_level = max(ignored_level, 0)
-            if is_block_raw is True and ignored_level == 0:
-                is_block_raw = False
+    def spacebefore(html):
+        return bool(re.search(r"[ ]*$",html, re.M))
 
-        if (
-            re.findall(
-                config.ignored_inline_blocks, item, flags=re.IGNORECASE | re.VERBOSE
-            )
-            and is_block_raw is False
-        ):
-            tmp = (indent * indent_level) + item + "\n"
+    output = ""
+    for el in elements:
+        #print(el)
 
-        # if a one-line, inline tag, just process it, only if line starts w/ it
-        elif (
-            re.findall(
-                rf"(<({slt_html})>)(.*?)(</(\2)>$)",
-                item,
-                re.IGNORECASE | re.VERBOSE | re.MULTILINE,
-            )
-            or re.findall(
-                re.compile(
-                    rf"(<({slt_html})\b.+?>)(.*?)(</(\2)>$)",
-                    re.IGNORECASE | re.VERBOSE | re.MULTILINE,
-                ),
-                item,
-            )
-            or re.findall(
-                rf"^({{%[ ]*?({slt_template})[ ]+?.+?%}})(.*?)({{%[ ]+?end(\2)[ ]+?.*?%}})",
-                item,
-                re.IGNORECASE | re.MULTILINE | re.VERBOSE,
-            )
-            or re.findall(
-                rf"(<({slt_html})\b.*?/>)", item, flags=re.IGNORECASE | re.VERBOSE
-            )
-            or re.findall(
-                re.compile(
-                    rf"(<({always_self_closing_html})\b.*?/?>)",
-                    re.IGNORECASE | re.VERBOSE,
-                ),
-                item,
-            )
-        ) and is_block_raw is False:
-            tmp = (indent * indent_level) + item + "\n"
+        if el.get("type") == "HTML":
+            attribs = (" " + (" ").join([x[0] + "=\"" + x[1] + "\"" for x in el["attrs"]]) + "" if el.get("attrs") else "")
 
-        # if unindent, move left
-        elif (
-            re.search(
-                config.tag_unindent,
-                item,
-                re.IGNORECASE | re.MULTILINE | re.VERBOSE,
-            )
-            and is_block_raw is False
-            and not is_safe_closing_tag(config, item)
-        ):
-            # block to catch inline block followed by a non-break tag
-            if (
-                len(
-                    re.findall(
-                        rf"(^<({slt_html})>)(.*?)(</(\2)>)",
-                        item,
-                        re.IGNORECASE | re.VERBOSE | re.MULTILINE,
-                    )
-                    or re.findall(
-                        re.compile(
-                            rf"(^<({slt_html})\b.+?>)(.*?)(</(\2)>)",
-                            re.IGNORECASE | re.VERBOSE | re.MULTILINE,
-                        ),
-                        item,
-                    )
-                )
-                > 0
-            ):
-                # unindent after instead of before
-                tmp = (indent * indent_level) + item + "\n"
-                indent_level = max(indent_level - 1, 0)
+
+            if el.get("tag") in indent_html_tags:
+                output = output.rstrip()
+                if output != "":
+                    output = output + "\n"
+
+                if el["location"] == "START":
+
+                    output += (indent * level) + "<" + el.get("tag") + attribs + ">\n"
+                    level += 1
+                elif el["location"] == "END":
+                    print("end")
+                    level -= 1
+                    output += (indent * level) + "</" + el.get("tag") + ">\n"
+                current_block = "tag"
+
+            elif el.get("tag") in ignored_html_tags:
+
+                if el["location"] == "START":
+                    if ignored_level == 0 and breakbefore(output):
+                        output += (indent * level)
+                    elif ignored_level == 0 and not breakbefore(output):
+                        output += "\n" + (indent * level)
+                    output += "<" + el.get("tag") + attribs+ ">"
+                    ignored_level += 1
+                elif el["location"] == "END":
+                    output += "</" + el.get("tag") + ">"
+                    ignored_level -= 1
+                current_block = "tag"
+
+
+            elif el.get("tag") in inline_blocks:
+
+                if el["location"] == "START":
+                    if inline_level == 0 and breakbefore(output):
+                        output += (indent * level)
+
+                    print(inline_level)
+                    print(output)
+                    print(breakbefore(output))
+                    print(current_block)
+                    if inline_level == 0 and not breakbefore(output) and current_block=="tag":
+                        output += "\n" + (indent * level)
+
+                    inline_level += 1
+                    output += "<" + el.get("tag") +attribs + ">"
+                elif el["location"] == "END":
+                    inline_level -= 1
+                    output += "</" + el.get("tag") + ">"
+
+                current_block = "tag"
+
+
+            elif el.get('comment'):
+                if breakbefore(output):
+                    output += (indent * level)
+                elif ignored_level == 0 and inline_level == 0 and not breakbefore(output):
+                    output += "\n" + (indent * level)
+                output += "<!--"+ el["comment"] + "-->"
+
             else:
-                indent_level = max(indent_level - 1, 0)
-                tmp = (indent * indent_level) + item + "\n"
+                assert 1==2
+                if el["location"] == "START":
+                    output += (indent * level) + "<" + el.get("tag") + attribs +">"
+                elif el["location"] == "END":
+                    output += "</" + el.get("tag") + ">"
+                current_block = "tag"
 
-        elif (
-            re.search(
-                r"^" + str(config.tag_unindent_line),
-                item,
-                re.IGNORECASE | re.MULTILINE | re.VERBOSE,
-            )
-            and is_block_raw is False
-        ):
-            tmp = (indent * (indent_level - 1)) + item + "\n"
 
-        # if indent, move right
-        elif (
-            re.search(
-                re.compile(
-                    r"^(?:" + str(config.tag_indent) + r")",
-                    re.IGNORECASE | re.MULTILINE | re.VERBOSE,
-                ),
-                item,
-            )
-            and is_block_raw is False
-        ):
-            tmp = (indent * indent_level) + item + "\n"
-            indent_level = indent_level + 1
 
-        elif is_raw_first_line is True or (
-            is_safe_closing_tag(config, item) and is_block_raw is False
-        ):
-            tmp = (indent * indent_level) + item + "\n"
+        elif el.get("type") == "CURLY_STATEMENT":
+            attribs = (" " + (" ").join(el["attrs"]) + "" if el.get("attrs") else "")
+            if el.get("tag") in indent_temp_tags:
+                if el["location"] == "START":
+                    output += (indent * level) + "{% " + el.get("tag") + attribs +" %}\n"
+                    level += 1
+                elif el["location"] == "END":
+                    level -= 1
+                    output += (indent * level) + "{% end" + el.get("tag") + " %}\n"
 
-        elif is_block_raw is True or item.strip() == "":
-            tmp = item + "\n"
 
-        # otherwise, just leave same level
-        else:
-            tmp = (indent * indent_level) + item + "\n"
+        elif el.get('data'):
+            if ignored_level > 0 or inline_level > 0 and el.get('data').strip() != "":
+                output += el.get('data')
+                current_block = "text"
+            elif el.get('data').strip() != "":
 
-        # if a opening raw tag then start ignoring.. only if there is no closing tag
-        # on the same line
-        if is_ignored_block_opening(config, item):
-            is_block_raw = True
-            is_raw_first_line = False
+                if breakbefore(output):
+                    output += (indent * level)
+                output += re.sub(r"\s+$", " ",el.get('data').lstrip(), re.M)
+                current_block = "text"
 
-        # if a normal tag, we can try to expand attributes
-        elif is_block_raw is False:
-            # get leading space, and attributes
-            func = partial(format_attributes, config, item)
 
-            tmp = re.sub(
-                re.compile(
-                    rf"(\s*?)(<(?:{config.indent_html_tags})\b)((?:\"[^\"]*\"|'[^']*'|{{[^}}]*}}|[^'\">{{}}])+?)(/?>)",
-                    re.VERBOSE | re.IGNORECASE,
-                ),
-                func,
-                tmp,
-            )
+    output = front_matter + output
 
-        # turn off raw block if we hit end - for one line raw blocks, but not an inline raw
-        if is_ignored_block_closing(config, item):
-            if not is_safe_closing_tag(config, item):
-                ignored_level -= 1
-                ignored_level = max(ignored_level, 0)
-            if ignored_level == 0:
-                is_block_raw = False
-
-        beautified_code = beautified_code + tmp
-
-    # we can try to fix template tags. ignore handlebars
-    if config.profile not in ["handlebars", "golang"]:
-
-        def fix_non_handlebars_template_tags(
-            html: str, out_format: str, match: re.Match
-        ) -> str:
-
-            if inside_ignored_block(config, html, match):
-                return match.group()
-
-            return out_format % (
-                match.group(1),
-                match.group(2),
-                match.group(3),
-            )
-
-        func = partial(fix_non_handlebars_template_tags, beautified_code, "%s %s%s")
-        beautified_code = re.sub(
-            r"({[{|%]\-?)(\w[^}].+?)([}|%]})", func, beautified_code
-        )
-
-        func = partial(fix_non_handlebars_template_tags, beautified_code, "%s%s %s")
-        beautified_code = re.sub(
-            r"({[{|%])([^}].+?[^(?:\ |\-)])([}|%]})", func, beautified_code
-        )
-
-        func = partial(fix_non_handlebars_template_tags, beautified_code, "%s%s %s")
-        beautified_code = re.sub(
-            r"({[{|%])([^}].+?[^ -])(\-+?[}|%]})", func, beautified_code
-        )
-
-    elif config.profile == "handlebars":
-
-        def fix_handlebars_template_tags(
-            html: str, out_format: str, match: re.Match
-        ) -> str:
-
-            if inside_ignored_block(config, html, match):
-                return match.group()
-
-            return out_format % (
-                match.group(1),
-                match.group(2),
-            )
-
-        func = partial(fix_handlebars_template_tags, beautified_code, "%s %s")
-        # handlebars templates
-        beautified_code = re.sub(r"({{#(?:each|if).+?[^ ])(}})", func, beautified_code)
-
-    return beautified_code.strip() + "\n"
+    return output
