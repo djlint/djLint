8e0b4b1b41c1b7e2e328ea379284c18b4acdb69e
Christopher Pickering <christopher.pickering@keemail.me>
2022-04-25 08:26:13 -0500

    fix(stdin and gitignore): updated to prevent gitignore from skipping std temp paths
    Disabled gitignore option for stdin paths.
    
    closes #224

--- tests/test_config_gitignore.py
+++ tests/test_config_gitignore.py
@@ -7,12 +7,13 @@ run::
 
 for a single test, run::
 
-   pytest tests/test_config_gitignore.py::test_pyproject --cov=src/djlint \
+   pytest tests/test_config_gitignore.py::test_ignored_path --cov=src/djlint \
      --cov-branch --cov-report xml:coverage.xml --cov-report term-missing
 
 """
 # pylint: disable=C0116
 import os
+import shutil
 from pathlib import Path
 
 from click.testing import CliRunner
@@ -40,6 +41,7 @@ def test_cli(runner: CliRunner) -> None:
     assert result.exit_code == 1
 
     os.remove("tests/config_gitignore/.gitignore")
+    shutil.rmtree("tests/config_gitignore/.git")
 
 
 def test_pyproject(runner: CliRunner) -> None:
@@ -73,3 +75,20 @@ def test_pyproject(runner: CliRunner) -> None:
 
     os.remove("tests/config_gitignore/.gitignore")
     os.remove("tests/config_gitignore/pyproject.toml")
+    shutil.rmtree("tests/config_gitignore/.git")
+
+def test_ignored_path(runner: CliRunner) -> None:
+    # test for https://github.com/Riverside-Healthcare/djLint/issues/224
+    # create .git folder to make root
+    Path("tests/config_gitignore/.git").mkdir(parents=True, exist_ok=True)
+    # add a gitignore file
+    with open("tests/config_gitignore/.gitignore", "w") as git:
+        git.write("var")
+
+    result = runner.invoke(djlint, ["-", "--use-gitignore"], input='<div><p id="a"></p></div>')
+    print(result.output)
+    assert result.exit_code == 0
+    assert "Linted 1 file" in result.output
+
+    os.remove("tests/config_gitignore/.gitignore")
+    shutil.rmtree("tests/config_gitignore/.git")
