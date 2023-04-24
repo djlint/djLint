1bcf1cbfeefa7f6ab7403b8c00e007b223ccce4c
Christopher Pickering <christopher@going.bg>
2023-04-24 08:26:52 -0500

    docs(netlify): removed package-lock from docs.. attempt to appease netlify
    re #607

--- docs/src/_data/css.js
+++
@@ -1,7 +0,0 @@
-const generateContentHash = require('../lib/generate-content-hash.js');
-
-const hash = generateContentHash('src/static/**/*.{scss,css}');
-
-module.exports = {
-  stylesCss: `/static/css/${hash}.css`,
-};
