f8fc285e08b657fd43989bfdfccf9e37395e5873
jlw4049 <jlw_4049@hotmail.com>
2023-07-16 18:00:25 -0400

    - Add both quote styles to the jinja parenthesis test

--- tests/test_jinja/test_parenthesis.py
+++ tests/test_jinja/test_parenthesis.py
@@ -9,16 +9,22 @@ from tests.conftest import printer
 
 test_data = [
     pytest.param(
-        ("{{ url('foo')}}"),
-        ("{{ url('foo') }}\n"),
+        "{{ url('foo') }}",
+        "{{ url('foo') }}\n",
+        '{{ url("foo") }}\n',
         id="parenthesis_tag",
-    ),
+    )
 ]
 
 
-@pytest.mark.parametrize(("source", "expected"), test_data)
-def test_base(source, expected, jinja_config):
+@pytest.mark.parametrize(("source", "expected1", "expected2"), test_data)
+def test_base(source, expected1, expected2, jinja_config):
     output = formatter(jinja_config, source)
 
-    printer(expected, source, output)
-    assert expected == output
+    printer(expected1, source, output)
+    assert expected1 == output or expected2 == output
+
+    output = formatter(jinja_config, source)
+
+    printer(expected2, source, output)
+    assert expected1 == output or expected2 == output
