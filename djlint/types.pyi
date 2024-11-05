d97be3b90e3cb81f0ac209b51c9d340f68694fe6
monosans <hsyqixco@protonmail.com>
2024-11-05 08:51:59 +0300

    fix: types.py -> types.pyi

--- djlint/types.pyi
+++ djlint/types.pyi
@@ -1,14 +1,10 @@
-from __future__ import annotations
-
 from typing_extensions import TypedDict
-
 
 class LintError(TypedDict):
     code: str
     line: str
     match: str
     message: str
-
 
 class ProcessResult(TypedDict, total=False):
     format_message: dict[str, tuple[str, ...]]
