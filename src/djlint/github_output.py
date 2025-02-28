1d296061240f0ffdc8fd7e58cd554db6724ea5d7
Michael Bianco <iloveitaly@gmail.com>
2025-02-28 04:42:57 -0700

    style: ruff format

--- src/djlint/github_output.py
+++ src/djlint/github_output.py
@@ -19,18 +19,21 @@ def print_github_output(
     format_error_count = 0
 
     for error in sorted(
-        file_errors,
-        key=lambda x: next(iter(next(iter(x.values())))),
+        file_errors, key=lambda x: next(iter(next(iter(x.values()))))
     ):
         if error.get("format_message") and not config.stdin:
-            format_error_count += print_format_errors(error["format_message"], config)
+            format_error_count += print_format_errors(
+                error["format_message"], config
+            )
         if error.get("lint_message"):
             lint_error_count += print_lint_errors(error["lint_message"], config)
 
     return lint_error_count + format_error_count
 
 
-def print_lint_errors(error: Mapping[str, Iterable[LintError]], config: Config) -> int:
+def print_lint_errors(
+    error: Mapping[str, Iterable[LintError]], config: Config
+) -> int:
     """Print lint errors in GitHub format."""
     errors = sorted(
         next(iter(error.values())),
@@ -51,7 +54,9 @@ def print_lint_errors(error: Mapping[str, Iterable[LintError]], config: Config) 
     return len(errors)
 
 
-def print_format_errors(errors: Mapping[str, Sequence[str]], config: Config) -> int:
+def print_format_errors(
+    errors: Mapping[str, Sequence[str]], config: Config
+) -> int:
     """Print format errors in GitHub format."""
     if not errors:
         return 0
