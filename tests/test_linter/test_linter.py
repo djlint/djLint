djlint/djLint

commit 04a6e138ececff5e65848282197bc55101e106d1

fix: avoid false H025 reports for multiline Go template attributes.

tests/test_linter/test_linter.py
@@ -387,6 +387,17 @@ def test_H025(
     result = runner.invoke(djlint, (tmp_file.name,))
     assert "H025" not in result.output
 
+    write_to_file(
+        tmp_file.name,
+        b"""<div
+    data-x="{{Func
+        "a"}}">
+</div>""",
+    )
+    result = runner.invoke(djlint, (tmp_file.name, "--profile=golang"))
+    assert result.exit_code == 0
+    assert "H025" not in result.output
+
     write_to_file(tmp_file.name, b"<col>")
     result = runner.invoke(djlint, (tmp_file.name,))
     assert "H025" not in result.output

src/djlint/settings.py
@@ -924,22 +924,22 @@ class Config:
                         \"[^\"]*? # double quoted attribute
                         (?:
                             {self.template_if_for_pattern} # if or for loop
-                           | {{{{.*?}}}} # template stuff
-                           | {{%[^}}]*?%}}
+                           | {{{{[\s\S]*?}}}} # template stuff
+                           | {{%[\s\S]*?%}}
                            | [^\"] # anything else
                         )*?
                         \" # closing quote
                       | '[^']*? # single quoted attribute
                         (?:
                             {self.template_if_for_pattern} # if or for loop
-                           | {{{{.*?}}}} # template stuff
-                           | {{%[^}}]*?%}}
+                           | {{{{[\s\S]*?}}}} # template stuff
+                           | {{%[\s\S]*?%}}
                            | [^'] # anything else
                         )*?
                         \' # closing quote
                       | (?:\w|-)+ # or a non-quoted string value
-                      | {{{{.*?}}}} # a non-quoted template var
-                      | {{%[^}}]*?%}} # a non-quoted template tag
+                      | {{{{[\s\S]*?}}}} # a non-quoted template var
+                      | {{%[\s\S]*?%}} # a non-quoted template tag
                       | {self.template_if_for_pattern} # a non-quoted if statement
 
                     )
@@ -949,9 +949,9 @@ class Config:
             """
             r"""
             | (?:\'|\") # allow random trailing quotes
-            | {{.*?}}
-            | {\#.*?\#}
-            | {%.*?%})
+            | {{[\s\S]*?}}
+            | {\#[\s\S]*?\#}
+            | {%[\s\S]*?%})
         """
         )
 

commit f3f42bb7b1c9cf3295a27ad8ed4845b74b64a964

fix: avoid H025 false positives after self-closing tags
Regression introduced in https://github.com/djlint/djLint/pull/2084

src/djlint/rules/H025.py
@@ -36,26 +36,32 @@ def run(
     orphan_tags: list[re.Match[str]] = []
 
     for match in re.finditer(
-        r"<(/?(\w+))\s*(" + config.attribute_pattern + r"|\s*)*\s*?>",
+        r"<(/?(\w+))\s*(" + config.attribute_pattern + r"|\s*)*\s*/?>",
         html,
         flags=re.X,
     ):
-        if match.group(1) and not re.search(
-            rf"^/?{config.always_self_closing_html_tags}\b",
-            match.group(1),
-            flags=RE_FLAGS_IX,
+        if (
+            match.group().rstrip().endswith("/>")
+            or not match.group(1)
+            or re.search(
+                rf"^/?{config.always_self_closing_html_tags}\b",
+                match.group(1),
+                flags=RE_FLAGS_IX,
+            )
         ):
-            # close tags should equal open tags
-            if match.group(1)[0] != "/":
-                open_tags.insert(0, match)
+            continue
+
+        # close tags should equal open tags
+        if match.group(1)[0] != "/":
+            open_tags.insert(0, match)
+        else:
+            for i, tag in enumerate(open_tags):
+                if tag.group(2) == match.group(1)[1:]:
+                    open_tags.pop(i)
+                    break
             else:
-                for i, tag in enumerate(open_tags):
-                    if tag.group(2) == match.group(1)[1:]:
-                        open_tags.pop(i)
-                        break
-                else:
-                    # there was no open tag matching the close tag
-                    orphan_tags.append(match)
+                # there was no open tag matching the close tag
+                orphan_tags.append(match)
 
     return tuple(
         {

tests/test_linter/test_linter.py
@@ -398,6 +398,28 @@ def test_H025(
     assert result.exit_code == 0
     assert "H025" not in result.output
 
+    write_to_file(
+        tmp_file.name,
+        b"""  <div class="list-group-item">
+    <h3 class="mb-3 text-center">
+    </h3>
+    <form method="post" action="{% url 'account_login' %}">
+        <input type="hidden"
+               value="{{ redirect_field_value }}" />
+      <div class="mb-3 text-center">
+      </div>
+      <button type="submit" class="btn btn-primary w-100">
+      </button>
+    </form>
+  </div>
+  <div class="list-group-item text-center">
+      <a href="{{ signup_url }}"
+         class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">sign up</a>.
+  </div>""",
+    )
+    result = runner.invoke(djlint, (tmp_file.name, "--profile=django"))
+    assert "H025" not in result.output
+
     write_to_file(tmp_file.name, b"<col>")
     result = runner.invoke(djlint, (tmp_file.name,))
     assert "H025" not in result.output